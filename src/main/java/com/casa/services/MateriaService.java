215fb0ab0b39145851e16d64eeed6ea1d6bbd646
Alex Dair Carrillo Ortiz <alex.carrillo@sofka.com.co>
2022-12-29 17:15:30 -0500

    Ajustando servicio de materia

--- src/main/java/com/casa/services/MateriaService.java
+++ src/main/java/com/casa/services/MateriaService.java
@@ -20,7 +20,7 @@ import com.casa.utils.Constantes;
 @Service
 public class MateriaService {
 	
-	private final Logger log = LoggerFactory.getLogger(DiaService.class);
+	private final Logger log = LoggerFactory.getLogger(MateriaService.class);
 	
 	@Autowired
 	private MateriaRepository materiaRepository;
